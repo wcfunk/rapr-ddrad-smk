f58e44dd422eea1f69f7e93af71bc6cfde05750a
Chris Funk (from Ovis) <Chris.Funk@colostate.edu>
2024-05-01 14:25:55 -0600

    added cstacks

---
+++ rules/cstacks.sh
@@ -0,0 +1,25 @@
+# Build the catalog of loci available in the metapopulation from the samples contained
+# in the population map.
+
+rule cstacks:
+    input:
+	directory="results/stacks_denovo"
+    output:
+	catalog-tags="results/stacks_denovo/catalog.tags.tsv.gz",
+	catalog-snps="results/stacks_denovo/catalog.snps.tsv.gz",
+	catalog-alleles="results/stacks_denovo/catalog.alleles.tsv.gz",
+	catalog-list="results/stacks_denovo/catalog.sample_list.tsv.gz"
+	cstacks-out="results/stacks_denovo/cstacks.out",
+	cstacks-err="results/stacks_denovo/cstacks.err"
+    conda:
+        "envs/stacks.yaml"
+ resources:
+	cpus=24,
+        mem_mb=lambda _, attempt: 54000 + ((attempt - 1) * 2000),
+        runtime_min=lambda _, attempt: 100 * (attempt),
+    log:
+        "results/logs/stacks_denovo/cstacks/{sample}.log"
+    shell:
+	" (cstacks -P {input.directory} -p 24	"
+	" -M data/popmap_lib2_6frogs.tsv	"
+	" -n 3) 2> {log}			"
